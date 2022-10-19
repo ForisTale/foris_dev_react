ae85607dcc223f8cdc7de503d9df5af465c924a9
Foris <foris4@gmail.com>
2022-10-19 17:11:12 +0100

    basic main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import MainPageLayout from "./components/layout/MainPageLayout";
 import TecPageLayout from "./components/layout/TecPageLayout";
 import {MainPageURLs} from "./inventory/mainPageURLs";
 import {Routes, Route} from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<MainPageLayout/>}>
-        <Route path={MainPageURLs.mainPage} element={<p>Main Page</p>}/>
+        <Route path={MainPageURLs.mainPage} element={<MainPage/>}/>
         <Route path={MainPageURLs.contact} element={<p>Contact</p>}/>
         <Route path={MainPageURLs.aboutMe} element={<p>About Me</p>}></Route>
       </Route>
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,37 @@
+import styles from "./MainPage.module.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Link} from "react-router-dom";
+import {MainPageURLs} from "../inventory/mainPageURLs";
+
+const MainPage = () => {
+  return (
+    <>
+      <Row className={"my-5"}>
+        <Col className={"my-5"}>
+          <h2>Purpose of this website:</h2>
+          <p>This site was created to demonstrate some of my django projects.<br/>
+            If you want to know more about my projects visit "About Me" page, where you can find link to my accounts.
+          </p>
+        </Col>
+      </Row>
+
+      <Row className={"my-5"}>
+        <Col className={"my-5"}>
+          <Link className={styles.link} to={MainPageURLs.tec}><h2>The Elder Commands</h2></Link>
+          <p>This site was created to demonstrate some of my django projects.<br/>
+            If you want to know more about my projects visit "About Me" page, where you can find link to my accounts.
+          </p>
+        </Col>
+      </Row>
+
+      <Row className={"my-5"}>
+        <Col className={"my-5"}>
+          <p>My projects are deployed using Ansible and tested with Selenium.</p>
+        </Col>
+      </Row>
+    </>
+);
+};
+
+export default MainPage;

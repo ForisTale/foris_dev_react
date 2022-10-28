7fdb1bd4e1e9f3f16ee71daa533c6f707cecd7d5
Foris <foris4@gmail.com>
2022-10-28 04:49:12 +0100

    utility to check if project is deployed

---
+++ utils/is_deployed.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+
+
+def is_deployed():
+    root_dir = Path(__file__).resolve().parent.parent
+    return os.path.exists(os.path.join(root_dir, "deployment_settings.yaml"))

71b415b4a73bbf47969382f8f4cfb721a153a5d0
Foris <foris4@gmail.com>
2022-11-10 04:33:30 +0000

    contact form validation and privacy policy agreement

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,28 +1,57 @@
 import Form from "react-bootstrap/Form";
 import classes from "./ContactForm.module.css";
 import Button from "react-bootstrap/Button";
-import {useState} from "react";
+import {useState, useReducer, useEffect} from "react";
 import getCSRFToken from "../inventory/getCSRFToken";
 import {importantMessagesActions} from "../store/importantMessages-slice";
 import {useDispatch} from "react-redux";
+import {Link} from "react-router-dom";
+import {MainPageURLs} from "../inventory/mainPageURLs";
 
+const initialInputState = {
+  text: "",
+  isValid: false,
+};
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "input":
+      return {text: action.text, isValid: action.text !== ""};
+    default :
+      throw new Error();
+  }
+};
 
 function ContactForm() {
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const [email, emailDispatch] = useReducer(inputReducer, initialInputState);
+  const [subject, subjectDispatch] = useReducer(inputReducer, initialInputState);
+  const [message, messageDispatch] = useReducer(inputReducer, initialInputState);
+  const [agreement, setAgreement] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    const formValidation = () => {
+      return [subject.isValid, message.isValid, agreement].every(valid => valid);
+    };
+    setFormIsValid(formValidation());
+
+  }, [subject.isValid, message.isValid, agreement]);
+
   const emailHandler = (event) => {
-    setEmail(event.target.value);
+    emailDispatch({type: "input", text: event.target.value});
   };
 
   const subjectHandler = (event) => {
-    setSubject(event.target.value);
+    subjectDispatch({type: "input", text: event.target.value});
   };
 
   const messageHandler = (event) => {
-    setMessage(event.target.value);
+    messageDispatch({type: "input", text: event.target.value});
+  };
+
+  const checkboxHandler = () => {
+    setAgreement(prevState => !prevState);
   };
 
   const submitHandler = () => {
@@ -50,7 +79,12 @@ function ContactForm() {
     <Form className={classes.input}>
       <Form.Group className={classes.formMaxWidth} controlId={"formSubject"}>
         <Form.Label>Subject:</Form.Label>
-        <Form.Control className={classes.input} value={subject} onChange={subjectHandler}/>
+        <Form.Control
+          className={classes.input}
+          value={subject.text}
+          onChange={subjectHandler}
+          required
+        />
       </Form.Group>
       <Form.Group className={classes.formMaxWidth} controlId={"formEmail"}>
         <Form.Label>Email address:</Form.Label>
@@ -58,24 +92,29 @@ function ContactForm() {
           type={"email"}
           placeholder={"Optional"}
           className={classes.input}
-          value={email}
+          value={email.text}
           onChange={emailHandler}
         />
-        <Form.Text className={"text-muted"}>
-          We'll never share your e-mail with anyone else.
-        </Form.Text>
       </Form.Group>
       <Form.Group controlId={"formMessage"}>
         <Form.Label>Message:</Form.Label>
         <Form.Control
           as={"textarea"}
           className={classes.input}
-          value={message}
+          value={message.text}
           onChange={messageHandler}
         />
       </Form.Group>
+      <Form.Group>
+        <Form.Check
+          className={classes.checkbox}
+          type={"checkbox"}
+          label={<p>I have read and agree to <Link to={MainPageURLs.privacyPolicy}>Privacy Policy</Link></p>}
+          onChange={checkboxHandler}
+        />
+      </Form.Group>
       <Form.Group className={classes.formButton}>
-        <Button className={classes.button} onClick={submitHandler}>Submit</Button>
+        <Button disabled={!formIsValid} className={classes.button} onClick={submitHandler}>Submit</Button>
       </Form.Group>
     </Form>
   );

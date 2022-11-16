63bce417aa33a8b2527fe8a5dc881cae1b1ca6b1
Foris <foris4@gmail.com>
2022-11-16 16:02:40 +0000

    calculate default level

--- frontend/src/components/tec/skills/EstimatedLevels.jsx
+++ frontend/src/components/tec/skills/EstimatedLevels.jsx
@@ -1,7 +1,20 @@
+import calculateDefaultLevel from "./calculateDefaultLevel";
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+
 const EstimatedLevels = () => {
+  const race = useSelector(state => state.tecSkills.race);
+  const skills = useSelector(state => state.tecSkills.skills);
+  const [baseLevel, setBaseLevel] = useState(1);
+
+  useEffect(() => {
+    setBaseLevel(calculateDefaultLevel(race, skills));
+  }, [race, skills])
+
+
   return (
     <>
-      <h6 id={"base_level"}>Base level: 1</h6>
+      <h6 id={"base_level"}>Base level: {baseLevel}</h6>
       <h6 id={"estimated_desired_level"}>Estimated desired level: 1</h6>
     </>
   );

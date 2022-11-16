835891729b242cd0f21cb9e450065f9bc35a3492
Foris <foris4@gmail.com>
2022-11-16 16:26:39 +0000

    calculate desired level

--- frontend/src/components/tec/skills/EstimatedLevels.jsx
+++ frontend/src/components/tec/skills/EstimatedLevels.jsx
@@ -1,4 +1,5 @@
 import calculateDefaultLevel from "./calculateDefaultLevel";
+import calculateDesiredLevel from "./calculateDesiredLevel";
 import {useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 
@@ -6,16 +7,18 @@ const EstimatedLevels = () => {
   const race = useSelector(state => state.tecSkills.race);
   const skills = useSelector(state => state.tecSkills.skills);
   const [baseLevel, setBaseLevel] = useState(1);
+  const [desiredLevel, setDesiredLevel] = useState(1);
 
   useEffect(() => {
     setBaseLevel(calculateDefaultLevel(race, skills));
-  }, [race, skills])
+    setDesiredLevel(calculateDesiredLevel(race, skills));
+  }, [race, skills]);
 
 
   return (
     <>
       <h6 id={"base_level"}>Base level: {baseLevel}</h6>
-      <h6 id={"estimated_desired_level"}>Estimated desired level: 1</h6>
+      <h6 id={"estimated_desired_level"}>Estimated desired level: {desiredLevel}</h6>
     </>
   );
 };
